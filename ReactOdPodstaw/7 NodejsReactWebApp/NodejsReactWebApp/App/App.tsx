a99cd496756bf5dadcf46cfb60e2075a11f7ad57
Grzegorz Chmiel <grzegorzchmiel@poczta.onet.pl>
2019-06-17 16:21:27 +0200

    Przegotowanie pustej solucji do lekcji: 15. Text input cz. 1

--- ReactOdPodstaw/7 NodejsReactWebApp/NodejsReactWebApp/App/App.tsx
+++ ReactOdPodstaw/7 NodejsReactWebApp/NodejsReactWebApp/App/App.tsx
@@ -1,39 +1,5 @@
 import * as React from "react";
 import * as ReactDOM from "react-dom";
 
-interface AppProps {
-   //propsBtnName: string
-}
-class App extends React.Component<AppProps, {}> {
-   state = {
-      text: "",
-      //stateBtnName: "State Dodaj cyfre"
-   };
 
-   handleClick = () => {
-      const number = Math.floor(Math.random() * 10)
-      this.setState({
-         text: this.state.text + number
-      });
-   };
-
-   render() {
-      const varBtnName = "Zmienna dodaj cyfre"
-      return (
-         <React.Fragment>
-            {/*<button onClick={this.handleClick}>{this.state.stateBtnName}</button>*/}
-            {/*<button onClick={this.handleClick}>{this.props.propsBtnName}</button>*/}
-            <button onClick={this.handleClick}>{varBtnName}</button>
-            {/*<button onClick={this.handleClick}>Dodaj cyfre</button>*/}
-            <PanelResult text={this.state.text}>BB</PanelResult>
-         </React.Fragment>
-      );
-   }
-}
-
-const PanelResult = (props) => {
-   return <h1>{props.text}  +  {props.children}</h1>;
-};
-
-//ReactDOM.render(<App propsBtnName = "Props Dodaj cyfre" />, document.getElementById("root"));
-ReactDOM.render(<App />, document.getElementById("root"));
+ReactDOM.render([], document.getElementById("root"));
--- ReactOdPodstaw/7 NodejsReactWebApp/NodejsReactWebApp/dist/app-bundle.js
+++ ReactOdPodstaw/7 NodejsReactWebApp/NodejsReactWebApp/dist/app-bundle.js
@@ -95,53 +95,9 @@
 
 "use strict";
 
-var __extends = (this && this.__extends) || (function () {
-    var extendStatics = function (d, b) {
-        extendStatics = Object.setPrototypeOf ||
-            ({ __proto__: [] } instanceof Array && function (d, b) { d.__proto__ = b; }) ||
-            function (d, b) { for (var p in b) if (b.hasOwnProperty(p)) d[p] = b[p]; };
-        return extendStatics(d, b);
-    };
-    return function (d, b) {
-        extendStatics(d, b);
-        function __() { this.constructor = d; }
-        d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
-    };
-})();
 Object.defineProperty(exports, "__esModule", { value: true });
-var React = __webpack_require__(/*! react */ "./node_modules/react/index.js");
 var ReactDOM = __webpack_require__(/*! react-dom */ "./node_modules/react-dom/index.js");
-var App = /** @class */ (function (_super) {
-    __extends(App, _super);
-    function App() {
-        var _this = _super !== null && _super.apply(this, arguments) || this;
-        _this.state = {
-            text: "",
-        };
-        _this.handleClick = function () {
-            var number = Math.floor(Math.random() * 10);
-            _this.setState({
-                text: _this.state.text + number
-            });
-        };
-        return _this;
-    }
-    App.prototype.render = function () {
-        var varBtnName = "Zmienna dodaj cyfre";
-        return (React.createElement(React.Fragment, null,
-            React.createElement("button", { onClick: this.handleClick }, varBtnName),
-            React.createElement(PanelResult, { text: this.state.text }, "BB")));
-    };
-    return App;
-}(React.Component));
-var PanelResult = function (props) {
-    return React.createElement("h1", null,
-        props.text,
-        "  +  ",
-        props.children);
-};
-//ReactDOM.render(<App propsBtnName = "Props Dodaj cyfre" />, document.getElementById("root"));
-ReactDOM.render(React.createElement(App, null), document.getElementById("root"));
+ReactDOM.render([], document.getElementById("root"));
 
 
 /***/ }),
