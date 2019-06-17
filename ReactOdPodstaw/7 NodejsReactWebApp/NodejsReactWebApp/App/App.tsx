53601cd845bd115dbf30540ee096c36596aedb4c
Grzegorz Chmiel <grzegorzchmiel@poczta.onet.pl>
2019-06-17 21:50:24 +0200

    Końcowa wersja solucji dla lekacji: 15. Text input cz. 1

--- ReactOdPodstaw/7 NodejsReactWebApp/NodejsReactWebApp/App/App.tsx
+++ ReactOdPodstaw/7 NodejsReactWebApp/NodejsReactWebApp/App/App.tsx
@@ -1,5 +1,53 @@
 import * as React from "react";
 import * as ReactDOM from "react-dom";
 
+class App extends React.Component {
 
-ReactDOM.render([], document.getElementById("root"));
+   state = {
+      inputValue: ""
+   }
+
+   handleResetClick = (evt) => {
+      this.setState(this.resetState, this.resetStateCallback)
+   }
+
+   handleInputChange = (evt) => {
+      let value = evt.target.value.toUpperCase()
+      this.setState((prevState, props) => {
+         return ({
+            inputValue: value
+         })
+      }, this.inputChangeCallback)
+   }
+
+   inputChange = (prevState, props) => {
+
+   }
+
+   inputChangeCallback = () => {
+
+   }
+
+   resetState = (prevState, props) => {
+      return ({
+         inputValue: "value cancelled"
+      })
+   }
+
+   resetStateCallback = () => {
+
+   }
+
+   render() {
+      return (
+         <React.Fragment>
+            <input type="text" onChange={this.handleInputChange} value={this.state.inputValue} placeholder="Wpisz tekst..." />
+            <button type="reset" onClick={this.handleResetClick}>Reset</button>
+            <h1 className="title">{this.state.inputValue}</h1>
+         </React.Fragment>
+      )
+   }
+}
+
+
+ReactDOM.render(<App />, document.getElementById("root"));
