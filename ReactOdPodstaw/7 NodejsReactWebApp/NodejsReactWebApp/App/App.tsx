27f5e7a1ab2a90535088cda18f1a8c0bb993078d
Grzegorz Chmiel <grzegorzchmiel@poczta.onet.pl>
2019-08-05 20:34:16 +0200

    Koniec lekcji: 20. Zliczanie kliknięć cz. 2

--- ReactOdPodstaw/7 NodejsReactWebApp/NodejsReactWebApp/App/App.tsx
+++ ReactOdPodstaw/7 NodejsReactWebApp/NodejsReactWebApp/App/App.tsx
@@ -1,5 +1,116 @@
 import * as React from "react";
 import * as ReactDOM from "react-dom";
 
+interface CounterProps {
+   initialResult: number
+}
 
-ReactDOM.render([], document.getElementById("root"));
+class Counter extends React.Component<CounterProps> {
+
+   state = {
+      count: 0,
+      result: this.props.initialResult
+   };
+
+   /*handleMathClick(type, number) {
+      switch (type) {
+         case "subtraction":
+            this.setState((prevState) => this.makeSubtraction(prevState, number));
+            break;
+         case "addition":
+            this.setState((prevState) => this.makeAddition(prevState, number));
+            break;
+         default:
+            //Reset
+            this.setState((prevState) => this.makeReset(prevState, number));
+      }
+   }*/
+
+   handleMathClick = (type, number) => {
+      switch (type) {
+         case "subtraction":
+            this.setState((prevState) => this.makeSubtraction(prevState, number));
+            break;
+         case "addition":
+            this.setState((prevState) => this.makeAddition(prevState, number));
+            break;
+         default:
+            //Reset
+            this.setState((prevState) => this.makeReset(prevState, number));
+      };
+   }
+
+   makeAddition(prevState, number) {
+      return ({
+         count: prevState.count + 1,
+         result: prevState.result + number
+      });
+   }
+
+   makeSubtraction(prevState, number) {
+      return ({
+         count: prevState.count + 1,
+         result: prevState.result - number
+      });
+   }
+
+   makeReset(prevState, number) {
+      return ({
+         count: prevState.count + 1,
+         result: this.props.initialResult
+      });
+   }
+
+   /*makeAddition = (prevState, number) => {
+      return ({
+         count: prevState.count + 1,
+         result: prevState.result + number
+      });
+   };
+
+   makeSubtraction = (prevState, number) => {
+      return ({
+         count: prevState.count + 1,
+         result: prevState.result - number
+      });
+   };
+
+   makeReset = (prevState, number) => {
+      return ({
+         count: prevState.count + 1,
+         result: this.props.initialResult
+      });
+   };*/
+
+   render() {
+      return (
+         <React.Fragment>
+            {/*<MathButton text="-10" type="subtraction" number="10" clickHandler={this.handleMathClick.bind(this)}></MathButton>
+            <MathButton text="-1" type="subtraction" number="1" clickHandler={this.handleMathClick.bind(this)}></MathButton>
+            <MathButton text="Reset" type="reset" clickHandler={this.handleMathClick.bind(this)}></MathButton>
+            <MathButton text="+1" type="addition" number="1" clickHandler={this.handleMathClick.bind(this)}></MathButton>
+            <MathButton text="+10" type="addition" number="10" clickHandler={this.handleMathClick.bind(this)}></MathButton>*/}
+
+            <MathButton text="-10" type="subtraction" number="10" clickHandler={this.handleMathClick}></MathButton>
+            <MathButton text="-1" type="subtraction" number="1" clickHandler={this.handleMathClick}></MathButton>
+            <MathButton text="Reset" type="reset" clickHandler={this.handleMathClick}></MathButton>
+            <MathButton text="+1" type="addition" number="1" clickHandler={this.handleMathClick}></MathButton>
+            <MathButton text="+10" type="addition" number="10" clickHandler={this.handleMathClick}></MathButton>
+
+            <h1>Ilosc klikniec: {this.state.count}</h1>
+            <h1>Rezultat: {this.state.result}</h1>
+         </React.Fragment>
+      );
+   }
+
+}
+
+const MathButton = (props) => {
+   let numberValue = parseInt(props.number)
+   return (
+      <button onClick={() => props.clickHandler(props.type, numberValue)}>{props.text}</button>
+   );
+};
+
+let initialResult = 8;
+ReactDOM.render(<Counter initialResult={initialResult} />, document.getElementById("root"));
