6e45bb91c3a48f0afb5aba8c2589ceb6c647e40a
Grzegorz Chmiel <grzegorzchmiel@poczta.onet.pl>
2019-09-22 19:53:45 +0200

    Koniec lekcji: 34. Koszyk cz. 1

--- ReactOdPodstaw/7 NodejsReactWebApp/NodejsReactWebApp/App/App.tsx
+++ ReactOdPodstaw/7 NodejsReactWebApp/NodejsReactWebApp/App/App.tsx
@@ -1,4 +1,41 @@
 import * as React from "react";
 import * as ReactDOM from "react-dom";
 
-ReactDOM.render([], document.getElementById("root"));
+class App extends React.Component {
+	state = {
+		availableProducts: 15,
+		shoppingCart: 0
+	}
+
+	caller = ""
+
+	handleAdd = () => {
+		this.caller = "Add"
+		this.setState(this.changeShoppingCard);
+	}
+
+	handleRem = () => {
+		this.caller = "Rem"
+		this.setState(this.changeShoppingCard);
+	}
+
+	changeShoppingCard(prevState) {
+		const quantity = this.caller == "Add" ? 1 : -1;
+		const returnObj = { shoppingCart: prevState.shoppingCart + quantity }
+		return returnObj
+	}
+
+	render() {
+		const addDisabled = this.state.shoppingCart == this.state.availableProducts
+		const remDisabled = this.state.shoppingCart == 0
+		return (
+			<React.Fragment>
+				<button id="Add" onClick={this.handleAdd} disabled={addDisabled}>+</button>
+				<span> {this.state.shoppingCart} </span>
+				<button id="Rem" onClick={this.handleRem} disabled={remDisabled}>-</button>
+			</React.Fragment>
+		)
+	}
+}
+
+ReactDOM.render(<App />, document.getElementById("root"));
--- ReactOdPodstaw/7 NodejsReactWebApp/NodejsReactWebApp/dist/app-bundle.js
+++ ReactOdPodstaw/7 NodejsReactWebApp/NodejsReactWebApp/dist/app-bundle.js
@@ -95,9 +95,60 @@
 
 "use strict";
 
+var __extends = (this && this.__extends) || (function () {
+    var extendStatics = function (d, b) {
+        extendStatics = Object.setPrototypeOf ||
+            ({ __proto__: [] } instanceof Array && function (d, b) { d.__proto__ = b; }) ||
+            function (d, b) { for (var p in b) if (b.hasOwnProperty(p)) d[p] = b[p]; };
+        return extendStatics(d, b);
+    };
+    return function (d, b) {
+        extendStatics(d, b);
+        function __() { this.constructor = d; }
+        d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
+    };
+})();
 Object.defineProperty(exports, "__esModule", { value: true });
+var React = __webpack_require__(/*! react */ "./node_modules/react/index.js");
 var ReactDOM = __webpack_require__(/*! react-dom */ "./node_modules/react-dom/index.js");
-ReactDOM.render([], document.getElementById("root"));
+var App = /** @class */ (function (_super) {
+    __extends(App, _super);
+    function App() {
+        var _this = _super !== null && _super.apply(this, arguments) || this;
+        _this.state = {
+            availableProducts: 15,
+            shoppingCart: 0
+        };
+        _this.caller = "";
+        _this.handleAdd = function () {
+            _this.caller = "Add";
+            _this.setState(_this.changeShoppingCard);
+        };
+        _this.handleRem = function () {
+            _this.caller = "Rem";
+            _this.setState(_this.changeShoppingCard);
+        };
+        return _this;
+    }
+    App.prototype.changeShoppingCard = function (prevState) {
+        var quantity = this.caller == "Add" ? 1 : -1;
+        var returnObj = { shoppingCart: prevState.shoppingCart + quantity };
+        return returnObj;
+    };
+    App.prototype.render = function () {
+        var addDisabled = this.state.shoppingCart == this.state.availableProducts;
+        var remDisabled = this.state.shoppingCart == 0;
+        return (React.createElement(React.Fragment, null,
+            React.createElement("button", { id: "Add", onClick: this.handleAdd, disabled: addDisabled }, "+"),
+            React.createElement("span", null,
+                " ",
+                this.state.shoppingCart,
+                " "),
+            React.createElement("button", { id: "Rem", onClick: this.handleRem, disabled: remDisabled }, "-")));
+    };
+    return App;
+}(React.Component));
+ReactDOM.render(React.createElement(App, null), document.getElementById("root"));
 
 
 /***/ }),
