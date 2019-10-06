9526ccfddb3767a5f7e166a430d926732d3f1296
Grzegorz Chmiel <grzegorzchmiel@poczta.onet.pl>
2019-10-06 21:34:55 +0200

    Koniec lekcji 49: Przelicznik - cz.1

--- Sekcja_5_Formularze/app.js
+++ Sekcja_5_Formularze/app.js
@@ -1,6 +1,65 @@
+const ResultViewer = props => {
+  let { labelText, ratio, value } = props;
+  let displayValue = value / ratio;
+  const fixedDisplayValue = displayValue.toFixed(2);
+  displayValue = displayValue <= 0 ? "" : fixedDisplayValue;
+  return (
+    <>
+      <span>
+        {`${labelText}`} <strong>{`${displayValue}`}</strong>
+      </span>
+    </>
+  );
+};
+
 class ExchangeCounter extends React.Component {
+  get euroRatio() {
+    return 4.2;
+  }
+
+  get dollarRatio() {
+    return 3.5;
+  }
+
+  state = {
+    currentValue: 0
+  };
+
+  handleInputValueChange(event) {
+    this.setState({
+      currentValue: event.target.value
+    });
+  }
+
   render() {
-    return <></>;
+    return (
+      <>
+        <div>
+          <label htmlFor="inputValue">Podaj wartość do przeliczenia: </label>
+          <input
+            id="inputValue"
+            type="number"
+            onChange={() => {
+              this.handleInputValueChange(event);
+            }}
+          />
+        </div>
+        <div>
+          <ResultViewer
+            labelText="Wartość w $:"
+            ratio={this.dollarRatio}
+            value={this.state.currentValue}
+          ></ResultViewer>
+        </div>
+        <div>
+          <ResultViewer
+            labelText="Wartość w EUR:"
+            ratio={this.euroRatio}
+            value={this.state.currentValue}
+          ></ResultViewer>
+        </div>
+      </>
+    );
   }
 }
 
