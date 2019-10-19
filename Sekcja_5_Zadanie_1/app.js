d4302bcada011161fdda11730fc6f74da7e68611
Grzegorz Chmiel <grzegorzchmiel@poczta.onet.pl>
2019-10-19 19:21:58 +0200

    Lekcja 52: Zadanie 1 - zakończenie wersji pierwszej.

--- Sekcja_5_Zadanie_1/app.js
+++ Sekcja_5_Zadanie_1/app.js
@@ -1,1 +1,39 @@
-ReactDOM.render([], document.getElementById("root"));
+class ShowAugur extends React.Component {
+  augurs = ["Wróżba 1", "Wróżba 2", "Wróżba 3"];
+
+  state = {
+    currentIndex: 0
+  };
+
+  handleShowButtonClick() {
+    const augursSize = this.augurs.length;
+    const randomIndex = this.getRandomNumber(augursSize - 1);
+    this.setState({
+      currentIndex: randomIndex
+    });
+  }
+
+  getRandomNumber(maxValue) {
+    let result = 1;
+    do {
+      result = Math.random() * 10;
+      result = Math.floor(result);
+    } while (result > maxValue || result == this.state.currentIndex);
+    return result;
+  }
+
+  render() {
+    return (
+      <>
+        <button id="showButton" onClick={this.handleShowButtonClick.bind(this)}>
+          Pokaż wróżbę
+        </button>
+        <br />
+        <h3>Wybrana wróżba:</h3>
+        <h3>{this.augurs[this.state.currentIndex]}</h3>
+      </>
+    );
+  }
+}
+
+ReactDOM.render(<ShowAugur />, document.getElementById("root"));
