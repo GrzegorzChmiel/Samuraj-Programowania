d085902bbfbd3041b90117652791731a654878ea
Grzegorz Chmiel <grzegorzchmiel@poczta.onet.pl>
2019-10-22 21:40:22 +0200

    Koniec lekcji 55: Zadanie 2

--- Sekcja_5_Zadanie_2/app.js
+++ Sekcja_5_Zadanie_2/app.js
@@ -1,1 +1,52 @@
-ReactDOM.render([], document.getElementById("root"));
+const UserListItem = props => {
+  return (
+    <li>
+      <div>
+        {props.UserName} <button onClick={props.RemoveHandler}>Usuń</button>
+      </div>
+    </li>
+  );
+};
+
+class UserList extends React.Component {
+  state = {
+    users: ["Jan Nowak", "Piotr Kowalski", "Janusz Sawicki"]
+  };
+
+  createListItem = (id, user, handler) => {
+    return (
+      <UserListItem
+        key={id}
+        UserName={user}
+        RemoveHandler={handler}
+      ></UserListItem>
+    );
+  };
+
+  removeUserHandler = index => {
+    let users = this.state.users.map(x => x);
+    users.splice(index, 1);
+    this.setState({
+      users
+    });
+  };
+
+  render() {
+    const listItems = new Array(this.state.users.length);
+    for (let index = 0; index < listItems.length; index++) {
+      const element = this.state.users[index];
+      listItems[index] = this.createListItem(
+        index,
+        element,
+        this.removeUserHandler.bind(this, index)
+      );
+    }
+    return (
+      <>
+        <ul>{listItems}</ul>
+      </>
+    );
+  }
+}
+
+ReactDOM.render(<UserList />, document.getElementById("root"));
