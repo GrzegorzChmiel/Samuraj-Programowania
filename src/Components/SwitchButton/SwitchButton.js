eabb95107294c12fedac170540663bf9b4821a2c
Grzegorz Chmiel <grzegorzchmiel@poczta.onet.pl>
2019-11-20 08:14:29 +0100

    Przygotowanie do kolejnej lekcji

--- src/Components/SwitchButton/SwitchButton.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-const SwitchButton = props => {
-  return (
-    <button onClick={props.handler}>{props.isActive ? "Stop" : "Start"}</button>
-  );
-};
-
-export default SwitchButton;
