64781e2427dbf0693ffc2f21173a490ebb298b33
GrzegorzChmiel <GrzegorzChmiel@poczta.onet.pl>
2019-12-17 18:32:20 +0100

    Przygotowanie do kolejnego projektu

--- src/App.js
+++ src/App.js
@@ -1,61 +1,9 @@
-import React from "react";
+import React, { Component } from "react";
 import "./App.css";
 
-class App extends React.Component {
-  state = {
-    users: []
-  };
-
-  componentDidMount() {
-    fetch("https://jsonplaceholder.typicode.com/users")
-      .then(
-        response => {
-          return this.checkResponseStatus(response);
-        },
-        reason => {
-          console.log(reason);
-        }
-      )
-      .then(response => this.getJSON(response))
-      .then(response => this.updateState(response));
-  }
-
-  checkResponseStatus(response) {
-    if (response.status === 200) return response;
-
-    return null;
-  }
-
-  getJSON(response) {
-    if (response != null) return response.json();
-
-    return null;
-  }
-
-  updateState(json) {
-    let users = [];
-    if (json != null) {
-      users = json;
-    }
-
-    this.setState({ users });
-  }
-
-  createUserItem(user) {
-    return (
-      <div key={user.id}>
-        <h4>Osoba: {user.username}</h4>
-        <p>Miasto: {user.address.city}</p>
-      </div>
-    );
-  }
-
+class App extends Component {
   render() {
-    let usersToRender = [];
-    if (this.state.users.length > 0) {
-      usersToRender = this.state.users.map(user => this.createUserItem(user));
-    }
-    return <div className="App">{usersToRender}</div>;
+    return <div className="App"></div>;
   }
 }
 
