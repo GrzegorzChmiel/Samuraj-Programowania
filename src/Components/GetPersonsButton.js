e973c5f4dae91b83cfba9d598b8e505097ae2067
GrzegorzChmiel <GrzegorzChmiel@poczta.onet.pl>
2019-12-23 13:32:45 +0100

    Przygotowanie do kolejnego projektu

--- src/Components/GetPersonsButton.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const GetPersonsButton = props => {
-  return <button onClick={() => props.getHandler("Komunikat do konsoli -> Pobieram osoby")}>Pobierz kolejne 5 osób</button>;
-};
-
-export default GetPersonsButton;
