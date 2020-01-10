728970fdd0e337b883ed8106b152e27e82c24f0e
GrzegorzChmiel <GrzegorzChmiel@poczta.onet.pl>
2020-01-10 12:51:31 +0100

    S7 L84 Aplikacja pogoda cz 1

--- src/App.js
+++ src/App.js
@@ -1,26 +1,12 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+//https://www.youtube.com/watch?v=fMB7aNN5ot4&list=PLTs20Q-BTEMNlWqt1sofJj5HDDqNlZy3L&index=1
+
+class App extends React.Component {
+  render() {
+    return <div>Learn React</div>;
+  }
 }
 
 export default App;
