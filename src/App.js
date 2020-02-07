837eac4927cbd2ff62ed219de0ba47ec307f7c93
GrzegorzChmiel <GrzegorzChmiel@poczta.onet.pl>
2020-02-07 10:54:51 +0100

    S8 L86 - Zakończenie

--- src/App.js
+++ src/App.js
@@ -1,21 +1,85 @@
 import React from "react";
-import logo from "./logo.svg";
 import "./App.css";
 
 class App extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      user: "",
+      email: "",
+      pass: "",
+      accept: false
+    };
+
+    this.onChangeHandler = this.onChangeHandler.bind(this);
+    this.onSubmitHandler = this.onSubmitHandler.bind(this);
+  }
+
+  onChangeHandler(event) {
+    const targetType = event.target.type;
+    const targetName = event.target.name;
+    let newState = {};
+    if (targetType === "checkbox") {
+      newState = {
+        [targetName]: event.target.checked
+      };
+    } else {
+      newState = {
+        [targetName]: event.target.value
+      };
+    }
+
+    this.setState(prevState => {
+      return newState;
+    });
+  }
+
+  onSubmitHandler(event) {
+    event.preventDefault();
+  }
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
-            Learn React
-          </a>
-        </header>
-      </div>
+      <form onSubmit={this.onSubmitHandler} noValidate>
+        <label>
+          Twoje imię:
+          <input
+            type="text"
+            name="user"
+            value={this.state.user}
+            onChange={this.onChangeHandler}
+          ></input>
+        </label>
+        <label>
+          Twój email:
+          <input
+            type="email"
+            name="email"
+            value={this.state.email}
+            onChange={this.onChangeHandler}
+          ></input>
+        </label>
+        <label>
+          Twoje hasło:
+          <input
+            type="password"
+            name="pass"
+            value={this.state.pass}
+            onChange={this.onChangeHandler}
+          ></input>
+        </label>
+        <label>
+          Akceptacja harmonogramu {}
+          <input
+            type="checkbox"
+            name="accept"
+            checked={this.state.accept}
+            onChange={this.onChangeHandler}
+          ></input>
+        </label>
+        <button type="submit">Zatwierdź dane</button>
+      </form>
     );
   }
 }
