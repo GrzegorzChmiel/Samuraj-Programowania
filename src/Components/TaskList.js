7c456098c24be6244f5420f9b49791b17bcb348a
GrzegorzChmiel <GrzegorzChmiel@poczta.onet.pl>
2020-02-29 16:35:01 +0100

    S9 L94 Zakończenie

--- src/Components/TaskList.js
+++ src/Components/TaskList.js
@@ -6,18 +6,29 @@ function createTaskItem(task, makeDone, makeDelete) {
 }
 
 const TaskList = props => {
-   const TaskItems = props.tasks.map(task => createTaskItem(task, props.makeTaskDoneHandler, props.deleteTaskHandler));
+   const activeTasks = props.tasks.filter(task => task.active);
+   const activeTaskItems = activeTasks.map(task => createTaskItem(task, props.makeTaskDoneHandler, props.deleteTaskHandler));
+
+   const inactiveTasks = props.tasks.filter(task => !task.active);
+   const inactiveTaskItems = inactiveTasks.map(task => createTaskItem(task, props.makeTaskDoneHandler, props.deleteTaskHandler));
+
    return (
       <>
          <div className="active">
-            <h1>Zadania do zrobienia</h1>
-            <div>{TaskItems}</div>
+            <h1>
+               Zadania do zrobienia <em>({activeTaskItems.length})</em>
+            </h1>
+            {activeTaskItems.length === 0 && <h3>Brak aktywnych zadań do zrobienia</h3>}
+            <div>{activeTaskItems}</div>
          </div>
          <hr></hr>
 
          <div className="done">
-            <h3>Zadania zrobione</h3>
-            <div>{TaskItems}</div>
+            <h3>
+               Zadania zrobione <em>({inactiveTaskItems.length})</em>
+            </h3>
+            {inactiveTaskItems.length > 5 ? <h4>Widocznych jest tylko 5 ostatnich zadań</h4> : null}
+            <div>{inactiveTaskItems.slice(0, 5)}</div>
          </div>
          <hr></hr>
       </>
