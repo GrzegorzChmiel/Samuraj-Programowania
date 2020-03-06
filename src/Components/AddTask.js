2e77398b70a7500e8ea5002c17e10f2bd105d115
GrzegorzChmiel <GrzegorzChmiel@poczta.onet.pl>
2020-03-06 13:15:28 +0100

    S9 L96 Zakończenie

--- src/Components/AddTask.js
+++ src/Components/AddTask.js
@@ -1,11 +1,29 @@
 import React, { Component } from "react";
+import "./AddTask.css";
 
 class AddTask extends Component {
-   state = {};
+   state = {
+      newTaskName: "",
+      plannedDate: "",
+      priority: false
+   };
+
    render() {
       return (
          <>
-            <div>AddTask Component</div>
+            <div className="form">
+               <h1>TODO App</h1>
+               <label htmlFor="newTaskName">Nazwa zadania </label>
+               <input type="text" placeholder="Nowe zadanie" id="newTaskName" value={this.state.newTaskName}></input>
+               <br />
+               <label htmlFor="plannedDate">Planowana data wykonania </label>
+               <input type="date" id="plannedDate" value={this.state.plannedDate}></input>
+               <br />
+               <label htmlFor="priority">Priorytet </label>
+               <input type="checkbox" id="priority" checked={this.state.priority}></input>
+               <br />
+               <button>Dodaj zadanie</button>
+            </div>
             <hr></hr>
          </>
       );
