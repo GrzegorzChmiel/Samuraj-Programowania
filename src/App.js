381e5abc75534d912de0c36321135fea0b3853ca
GrzegorzChmiel <GrzegorzChmiel@poczta.onet.pl>
2020-03-21 09:05:06 +0100

    S10 L103 Zakończenie

--- src/App.js
+++ src/App.js
@@ -1,11 +1,23 @@
 import React from "react";
 import "./App.css";
-import { BrowserRouter, Link } from "react-router-dom";
+import { BrowserRouter as Router, Link, Route } from "react-router-dom";
+
+const Home = () => {
+   return <h3>Strona główna</h3>;
+};
+
+const News = () => {
+   return <h3>Aktualności</h3>;
+};
+
+const Contact = () => {
+   return <h3>Kontakt</h3>;
+};
 
 class App extends React.Component {
    render() {
       return (
-         <BrowserRouter>
+         <Router>
             <div className="App">
                <header className="App-header">
                   <nav>
@@ -14,7 +26,7 @@ class App extends React.Component {
                            <Link to="/">Strona główna</Link>
                         </li>
                         <li>
-                           <Link to="/News">News</Link>
+                           <Link to="/News">Aktualności</Link>
                         </li>
                         <li>
                            <Link to="/Contact">Kontakt</Link>
@@ -22,8 +34,13 @@ class App extends React.Component {
                      </ul>
                   </nav>
                </header>
+               <section>
+                  <Route path="/News" component={News}></Route>
+                  <Route path="/Contact" component={Contact}></Route>
+                  <Route exact path="/" component={Home}></Route>
+               </section>
             </div>
-         </BrowserRouter>
+         </Router>
       );
    }
 }
