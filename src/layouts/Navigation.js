349ca3c4a31555468fecacf47ebec85eca3ec0bf
GrzegorzChmiel <GrzegorzChmiel@poczta.onet.pl>
2020-03-27 21:45:55 +0100

    S10 L108 Zakończenie

--- src/layouts/Navigation.js
+++ src/layouts/Navigation.js
@@ -1,7 +1,31 @@
 import React from "react";
+import { NavLink } from "react-router-dom";
+import "../styles/Navigation.css";
+
+const menuItems = [
+   { itemName: "Start", to: "/", exact: true },
+   { itemName: "Produkty", to: "/products", exact: false },
+   { itemName: "Kontakt", to: "/contact", exact: false },
+   { itemName: "Panel administracyjny", to: "/admin", exact: false }
+];
+
+function convertToNavItem(item) {
+   return (
+      <li key={item.itemName}>
+         <NavLink to={item.to} exact={item.exact}>
+            {item.itemName}
+         </NavLink>
+      </li>
+   );
+}
 
 const Navigation = () => {
-   return <div>Navigation</div>;
+   const navItems = menuItems.map(item => convertToNavItem(item));
+   return (
+      <nav className="main">
+         <ul>{navItems}</ul>
+      </nav>
+   );
 };
 
 export default Navigation;
