847e97f3084db4e6c57fd0c560110ceee453cc6f
GrzegorzChmiel <GrzegorzChmiel@poczta.onet.pl>
2020-04-03 10:41:23 +0200

    S10 L110 Zakończenie

--- src/layouts/Page.js
+++ src/layouts/Page.js
@@ -1,7 +1,23 @@
 import React from "react";
+import HomePage from "../pages/HomePage";
+import ProductPage from "../pages/ProductPage";
+import ContactPage from "../pages/ContactPage";
+import AdminPage from "../pages/AdminPage";
+import ErrorPage from "../pages/ErrorPage";
+import { Route, Switch } from "react-router-dom";
 
 const Page = () => {
-   return <div>Page</div>;
+   return (
+      <>
+         <Switch>
+            <Route exact path="/" component={HomePage} a></Route>
+            <Route path="/products" component={ProductPage}></Route>
+            <Route path="/contact" component={ContactPage}></Route>
+            <Route path="/admin" component={AdminPage}></Route>
+            <Route component={ErrorPage}></Route>
+         </Switch>
+      </>
+   );
 };
 
 export default Page;
