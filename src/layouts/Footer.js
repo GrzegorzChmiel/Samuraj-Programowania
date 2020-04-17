09d7df36c402acab1e4c21ee7da29ad5307327b5
GrzegorzChmiel <GrzegorzChmiel@poczta.onet.pl>
2020-04-17 15:16:57 +0200

    S10 L115

--- src/layouts/Footer.js
+++ src/layouts/Footer.js
@@ -1,7 +1,68 @@
 import React from "react";
+import { Route } from "react-router-dom";
+import "../styles/Footer.css";
 
 const Footer = () => {
-   return <div>Footer</div>;
+   return (
+      <>
+         <h5>Stopka</h5>
+         <Route
+            path="/"
+            exact
+            render={(props) => (
+               <>
+                  <p>
+                     Page: <span>{props.match.params.page}</span>
+                  </p>
+                  <p>
+                     Url: <span>{props.match.url}</span>
+                  </p>
+                  <p>
+                     Path: <span>{props.match.path}</span>
+                  </p>
+               </>
+            )}
+         ></Route>
+
+         <Route
+            path="/:page"
+            exact
+            render={(props) => (
+               <>
+                  <p>
+                     Page: <span>{props.match.params.page}</span>
+                  </p>
+                  <p>
+                     Url: <span>{props.match.url}</span>
+                  </p>
+                  <p>
+                     Path: <span>{props.match.path}</span>
+                  </p>
+               </>
+            )}
+         ></Route>
+         <Route
+            path="/:page/:name"
+            exact
+            render={(props) => (
+               <>
+                  <p>
+                     Page: <span>{props.match.params.page}</span>
+                  </p>
+                  <p>
+                     Name: <span>{props.match.params.name}</span>
+                  </p>
+                  <p>
+                     Url: <span>{props.match.url}</span>
+                  </p>
+                  <p>
+                     Path: <span>{props.match.path}</span>
+                  </p>
+               </>
+            )}
+         ></Route>
+      </>
+   );
 };
 
 export default Footer;
